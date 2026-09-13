4a2e4525d9eb3583c0b532a0d4b4e1d6da1aeb27
lchh0818 <lchh8680@gmail.com>
2026-09-13 18:33:54 +0900

    수정

--- content.js
+++ content.js
@@ -6,7 +6,7 @@ window.portfolio = {
   name: 'ChanHyeok Lee', title: 'Product Engineer', role: [{ title: '한국디지털미디어고등학교 해킹방어과', period: '2024 - 현재' },{ title: 'IT 소셜벤처 창업동아리 LUNA 일반동아리장', period: '2025 - 2025' }, { title: '전국  IT고등학교 교류회 Link 대표', period: '2025 - 현재' }], photo: 'main.jpg', email: 'lchh8680@gmail.com', github: 'https://github.com/lchh0818', phone: '010-8680-0774',
   school: '한국디지털미디어 고등학교 해킹방어과',
   intro: 'I turn ideas into services and make them happen with people. Through web development and startup projects, I have focused on solving everyday problems for users.',
-  skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Next.js', 'Figma', 'Notion'],
+  skills: ['React', 'Next.js','flutter','React-Native','Yolo AI','TensorFlow','Figma', 'Notion'],
   projects: window.projectData,
   experiences: window.experienceData,
 };
